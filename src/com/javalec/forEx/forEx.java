39428b53d79631feb5cc34fb048851e90fb5f2c8
hadaslee <leesk55@gmail.com>
2015-04-28 15:50:44 +0900

    for 예제

---
+++ src/com/javalec/forEx/forEx.java
@@ -0,0 +1,13 @@
+package com.javalec.forEx;
+
+/**
+ * Created by sklee on 2015-04-28.
+ */
+public class forEx {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            int result = i + 10;
+            System.out.println("i는 " + i + " 입니다. \n" + "10 + i = " + result);
+        }
+    }
+}
