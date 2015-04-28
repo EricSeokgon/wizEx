0a1046fc78064ca754397eebd3ba4b071f2719ad
hadaslee <leesk55@gmail.com>
2015-04-28 14:38:46 +0900

    3의 배수 확인 계산기

---
+++ src/com/sklee/calex/CalEx.java
@@ -0,0 +1,16 @@
+package com.sklee.calex;
+
+import java.util.Scanner;
+
+/**
+ * Created by sklee on 2015-04-28.
+ */
+public class CalEx {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt();
+
+        System.out.println("i를 3으로 나누어서 나머지가 0이 나오면 사용자가 입력한 숫자는 3의 배수입니다.");
+        System.out.println("i % 3 =" + (i % 3));
+    }
+}
