238cae35244ee52ff7436516f9741e392c0a2975
hadaslee <leesk55@gmail.com>
2015-04-28 15:58:05 +0900

    배열 연습

---
+++ src/com/javalec/ex/WainClass.java
@@ -0,0 +1,19 @@
+package com.javalec.ex;
+
+/**
+ * Created by sklee on 2015-04-28.
+ */
+public class WainClass {
+    public static void main(String[] args) {
+        int[] iArr = {10, 20, 30, 40, 50};
+        System.out.println(iArr[4]);
+
+        int[] jArr = new int[3];
+        jArr[0] = 0;
+        jArr[1] = 1;
+        jArr[2] = 2;
+
+        System.out.println(jArr[1]);
+
+    }
+}
