79514d4634d8081f5aedb93393fe9de4819e971e
hadaslee <leesk55@gmail.com>
2015-04-29 13:26:50 +0900

    학생 클래스 작성

--- src/com/javalec/day9/manager/Student.java
+++ src/com/javalec/day9/manager/Student.java
@@ -8,4 +8,48 @@ package com.javalec.day9.manager;
  * To change this template use File | Settings | File Templates.
  */
 public class Student {
+
+    private String name;
+    private int age;
+    private int studentNum;
+    private String major;
+
+    public Student(String name, int age, int studentNum, String major) {
+        this.name = name;
+        this.age = age;
+        this.studentNum = studentNum;
+        this.major = major;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getStudentNum() {
+        return studentNum;
+    }
+
+    public void setStudentNum(int studentNum) {
+        this.studentNum = studentNum;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
 }
