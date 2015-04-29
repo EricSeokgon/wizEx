e1c19e95d2d5b524d08953569746bc09da950720
hadaslee <leesk55@gmail.com>
2015-04-29 13:18:18 +0900

    학사관리시스템 초기 파일 생성

---
+++ src/com/javalec/day9/manager/StudentExpel.java
@@ -0,0 +1,11 @@
+package com.javalec.day9.manager;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Hadeslee
+ * Date: 2015-04-29
+ * Time: 오후 1:15
+ * To change this template use File | Settings | File Templates.
+ */
+public class StudentExpel {
+}
