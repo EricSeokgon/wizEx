28a2a0428b4c023d79375d55154b8e07e40a14bb
hadaslee <leesk55@gmail.com>
2015-04-29 13:30:29 +0900

    재적학생 클래스 작성

--- src/com/javalec/day9/manager/StudentExpel.java
+++ src/com/javalec/day9/manager/StudentExpel.java
@@ -1,5 +1,7 @@
 package com.javalec.day9.manager;
 
+import java.util.ArrayList;
+
 /**
  * Created by IntelliJ IDEA.
  * User: Hadeslee
@@ -8,4 +10,16 @@ package com.javalec.day9.manager;
  * To change this template use File | Settings | File Templates.
  */
 public class StudentExpel {
+
+    ArrayList<Student> expelStudent;
+
+    public StudentExpel() {
+
+        expelStudent = new ArrayList<Student>();
+    }
+    public void addExpelStudent(String name, int age, int studentNem, String major){
+
+        expelStudent.add(new Student(name, age,studentNem,major));
+        System.out.println("재적 학생 정보 등록 완료");
+    }
 }
