062497e6cde68dea0659e16d8ab9f3bc09c2eecc
hadaslee <leesk55@gmail.com>
2015-05-01 16:07:23 +0900

    throws 학습

---
+++ src/com/javalec/day25/throwsex/mainClass.java
@@ -0,0 +1,18 @@
+package com.javalec.day25.throwsex;
+
+/**
+ * Project: wizEx
+ * FileName: mainClass
+ * Date: 2015-05-01
+ * Time: 오후 3:50
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class MainClass {
+
+    public static void main(String[] args) {
+
+        ThrowsExClass throwsExClass = new ThrowsExClass();
+    }
+}
