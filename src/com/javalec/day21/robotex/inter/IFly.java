5177d2b4e29093e44299f46ed594c7e851f8e9cf
hadaslee <leesk55@gmail.com>
2015-05-01 10:41:19 +0900

    로봇 인터페이스 구현

---
+++ src/com/javalec/day21/robotex/inter/IFly.java
@@ -0,0 +1,16 @@
+package com.javalec.day21.robotex.inter;
+
+/**
+ * Project: wizEx
+ * FileName: IFly
+ * Date: 2015-05-01
+ * Time: 오전 10:38
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public interface IFly {
+
+    void fly();
+
+}
---
+++ src/com/javalec/day21/robotex/inter/IMisail.java
@@ -0,0 +1,15 @@
+package com.javalec.day21.robotex.inter;
+
+/**
+ * Project: wizEx
+ * FileName: IMisail
+ * Date: 2015-05-01
+ * Time: 오전 10:39
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public interface IMisail {
+
+    void misail();
+}
