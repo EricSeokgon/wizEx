e6bc06159e28f026f911a3c4fc044d310ce0437b
hadaslee <leesk55@gmail.com>
2015-05-01 14:23:48 +0900

    Scanner 사용법

---
+++ src/com/javalec/day23/scannerex/ScannerEx.java
@@ -0,0 +1,29 @@
+package com.javalec.day23.scannerex;
+
+import java.util.Scanner;
+
+/**
+ * Project: wizEx
+ * FileName: ScannerEx
+ * Date: 2015-05-01
+ * Time: 오후 2:18
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class ScannerEx {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String str2 = scanner.nextLine();
+        System.out.println(str2);
+
+        String str1 = scanner.next();
+        System.out.println(str1);
+
+
+        System.out.println("END");
+
+    }
+}
