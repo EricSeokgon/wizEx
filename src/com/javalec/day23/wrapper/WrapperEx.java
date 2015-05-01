28d9c0dd16dfae5d75968c2745a4b29f2f26029a
hadaslee <leesk55@gmail.com>
2015-05-01 14:33:09 +0900

    기초데이터를 객체 데이터로 변환

---
+++ src/com/javalec/day23/wrapper/WrapperEx.java
@@ -0,0 +1,19 @@
+package com.javalec.day23.wrapper;
+
+/**
+ * Project: wizEx
+ * FileName: WrapperEx
+ * Date: 2015-05-01
+ * Time: 오후 2:27
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class WrapperEx {
+    public static void main(String[] args) {
+
+        Integer integer = new Integer(234); //기초데이터를 객체 데이터로 변환
+        int i = integer.intValue();  //int i = 234; (기초 데이터)
+        System.out.println("i = " + i);
+    }
+}
