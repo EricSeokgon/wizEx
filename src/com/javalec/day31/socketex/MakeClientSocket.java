0241d936942a87525baa7f9fec1cec6d8bb541f3
hadaslee <leesk55@gmail.com>
2015-05-06 17:57:48 +0900

    소켓통신 예제

---
+++ src/com/javalec/day31/socketex/MakeClientSocket.java
@@ -0,0 +1,48 @@
+package com.javalec.day31.socketex;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * Project: wizEx
+ * FileName: MakeClientSocket
+ * Date: 2015-05-06
+ * Time: 오후 5:45
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class MakeClientSocket {
+
+    Socket socket = null;
+    PrintWriter writer = null;
+    BufferedReader reader = null;
+
+    public MakeClientSocket() {
+        try {
+            socket = new Socket("localhost", 2000);
+            writer = new PrintWriter(socket.getOutputStream(), true);
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            String str = null;
+            BufferedReader sReader = new BufferedReader(new InputStreamReader(System.in));
+
+            while ((str = sReader.readLine()) != null) {
+                writer.print(str);
+                System.out.println("output : " + str);
+            }
+            writer.close();
+            reader.close();
+            sReader.close();
+            socket.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        new MakeClientSocket();
+    }
+}
---
+++ src/com/javalec/day31/socketex/MakeServerSocket.java
@@ -0,0 +1,55 @@
+package com.javalec.day31.socketex;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Project: wizEx
+ * FileName: MakeServerSocket
+ * Date: 2015-05-06
+ * Time: 오후 5:44
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class MakeServerSocket {
+
+    ServerSocket serverSocket = null;
+    Socket socket = null;
+    PrintWriter writer = null;
+    BufferedReader reader = null;
+    String lineStr;
+
+    public MakeServerSocket() {
+
+        try {
+            serverSocket = new ServerSocket(2000);
+            while (true) {
+                socket = serverSocket.accept();
+                System.out.println("Client 요청");
+
+                writer = new PrintWriter(socket.getOutputStream(), true);
+                reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                while ((lineStr = reader.readLine()) != null) {
+                    writer.write(lineStr);
+                    System.out.println("input : " + lineStr);
+                }
+                writer.close();
+                reader.close();
+                socket.close();
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        new MakeServerSocket();
+    }
+}
