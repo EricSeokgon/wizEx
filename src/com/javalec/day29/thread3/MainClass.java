48a6b4db32b8e60220f96ecbcceffa525f8b20ab
hadaslee <leesk55@gmail.com>
2015-05-06 15:34:35 +0900

    객체 1개 스데드 1개 예제

--- src/com/javalec/day29/thread3/MainClass.java
+++ src/com/javalec/day29/thread3/MainClass.java
@@ -12,10 +12,11 @@ package com.javalec.day29.thread3;
 public class MainClass {
     public static void main(String[] args) {
 
-        ThreadTest threadTest = new ThreadTest();
+        ThreadTest threadTest0 = new ThreadTest();
+        ThreadTest threadTest1 = new ThreadTest();
 
-        Thread thread0 = new Thread(threadTest, "A");
-        Thread thread1 = new Thread(threadTest, "B");
+        Thread thread0 = new Thread(threadTest0, "A");
+        Thread thread1 = new Thread(threadTest1, "B");
 
         thread0.start();
         thread1.start();
