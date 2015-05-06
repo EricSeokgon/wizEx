4b6633a33b1d35db9439f9cab0bec4a53626f600
hadaslee <leesk55@gmail.com>
2015-05-06 17:37:11 +0900

    urlconnection 테스트

---
+++ src/com/javalec/day31/urlconnectionex/MainCalss.java
@@ -0,0 +1,17 @@
+package com.javalec.day31.urlconnectionex;
+
+/**
+ * Project: wizEx
+ * FileName: MainCalss
+ * Date: 2015-05-06
+ * Time: 오후 5:18
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class MainCalss {
+    public static void main(String[] args) {
+
+        new URLConEx();
+    }
+}
---
+++ src/com/javalec/day31/urlconnectionex/URLConEx.java
@@ -0,0 +1,46 @@
+package com.javalec.day31.urlconnectionex;
+
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
+
+/**
+ * Project: wizEx
+ * FileName: URLConEx
+ * Date: 2015-05-06
+ * Time: 오후 5:18
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class URLConEx {
+
+    public URLConEx() {
+
+        String code = null;
+        System.out.println("웹 주소를 입력 하세요");
+        Scanner scanner = new Scanner(System.in);
+        String address = scanner.next();
+
+        try {
+            URL url = new URL(address);
+            URLConnection con = url.openConnection();
+            BufferedReader webData = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            FileWriter fw = new FileWriter("C:\\Users\\sklee\\IdeaProjects\\wizEx\\file.html", false);
+
+            while ((code = webData.readLine()) != null) {
+                fw.write(code);
+            }
+            System.out.println("The End");
+
+            fw.close();
+            webData.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
