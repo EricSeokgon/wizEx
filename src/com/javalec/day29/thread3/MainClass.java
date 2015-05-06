a685736568409a186653d4e92afb93cba1d5a630
hadaslee <leesk55@gmail.com>
2015-05-06 15:22:25 +0900

    객체 1개 스데드 n개 예제

---
+++ src/com/javalec/day29/thread3/MainClass.java
@@ -0,0 +1,26 @@
+package com.javalec.day29.thread3;
+
+/**
+ * Project: wizEx
+ * FileName: MainClass
+ * Date: 2015-05-06
+ * Time: 오후 1:45
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class MainClass {
+    public static void main(String[] args) {
+
+        ThreadTest threadTest = new ThreadTest();
+
+        Thread thread0 = new Thread(threadTest, "A");
+        Thread thread1 = new Thread(threadTest, "B");
+
+        thread0.start();
+        thread1.start();
+
+        System.out.println(Thread.currentThread().getName());
+        System.out.println("MainClass");
+    }
+}
