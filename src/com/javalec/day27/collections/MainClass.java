5034c971b86798c6ca3f531f6ee4ae7ee244d0d0
hadaslee <leesk55@gmail.com>
2015-05-06 10:09:21 +0900

    hashmap 학생 학년 예제

---
+++ src/com/javalec/day27/collections/MainClass.java
@@ -0,0 +1,24 @@
+package com.javalec.day27.collections;
+
+import java.util.HashSet;
+
+/**
+ * Project: wizEx
+ * FileName: MainClass
+ * Date: 2015-05-06
+ * Time: 오전 10:00
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class MainClass {
+    public static void main(String[] args) {
+
+        HashSet<Student> hashSet = new HashSet<Student>();
+        hashSet.add(new Student("홍길동", 3));
+        hashSet.add(new Student("이순신", 6));
+        hashSet.add(new Student("장보고", 1));
+
+        System.out.println(hashSet.toString());
+    }
+}
---
+++ src/com/javalec/day27/collections/Student.java
@@ -0,0 +1,28 @@
+package com.javalec.day27.collections;
+
+import com.javalec.day22.stringBuilderex.StringBuilderEx;
+
+/**
+ * Project: wizEx
+ * FileName: Student
+ * Date: 2015-05-06
+ * Time: 오전 10:00
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Student {
+
+    private String name;
+    private int grade;
+
+    public Student(String name, int grade) {
+        this.name = name;
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return name + " : " + grade;
+    }
+}
